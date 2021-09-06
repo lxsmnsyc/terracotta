ff16b8e2dd71982237525f318181fbfc18bf9388
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-09-06 22:07:09 +0800

    Update App.tsx

--- examples/listbox-example/src/App.tsx
+++ examples/listbox-example/src/App.tsx
@@ -63,7 +63,7 @@ export default function Example(): JSX.Element {
 
   return (
     <div class="w-72 fixed top-16">
-      <TailwindListbox value={selected()} onChange={setSelected}>
+      <TailwindListbox value={selected()} onSelectChange={setSelected}>
         <div class="relative mt-1">
           <TailwindListboxButton class="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
             <span class="block truncate">{selected().name}</span>
