046b6879accb52428c76058b18e578149c9c68a4
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-09-30 19:09:42 +0800

    Change animation

--- examples/dropdown-menu-example/src/App.tsx
+++ examples/dropdown-menu-example/src/App.tsx
@@ -63,11 +63,11 @@ export default function App(): JSX.Element {
             <TailwindTransition
               show={isOpen()}
               enter="transition duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
+              enterFrom="opacity-0 -translate-y-1 scale-50"
+              enterTo="opacity-100 translate-y-0 scale-100"
               leave="transition duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
+              leaveFrom="opacity-100 translate-y-0 scale-100"
+              leaveTo="opacity-0 -translate-y-1 scale-50"
             >
               <TailwindPopoverPanel unmount={false} class="absolute z-10 px-4 mt-3 transform -translate-x-1/2 left-1/2 sm:px-0 lg:max-w-3xl">
                 <TailwindMenu class="overflow-hidden w-64 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white flex flex-col space-y-1 p-1">
