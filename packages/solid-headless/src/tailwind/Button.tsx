896f607c53eafacb9680a9b6da12a4a82d06fb56
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-10-01 14:36:09 +0800

    Improve ref types for `Button`

--- packages/solid-headless/src/tailwind/Button.tsx
+++ packages/solid-headless/src/tailwind/Button.tsx
@@ -1,24 +1,41 @@
-import { createEffect, createSignal, createUniqueId, onCleanup } from 'solid-js';
-import { JSX } from 'solid-js/jsx-runtime';
-import { Dynamic } from 'solid-js/web';
-import { DynamicProps, ValidConstructor } from '../utils/dynamic-prop';
-import { excludeProps } from '../utils/exclude-props';
+import {
+  createEffect,
+  createSignal,
+  createUniqueId,
+  onCleanup,
+} from 'solid-js';
+import {
+  JSX,
+} from 'solid-js/jsx-runtime';
+import {
+  Dynamic,
+} from 'solid-js/web';
+import {
+  createRef,
+  DynamicNode,
+  DynamicProps,
+  ValidConstructor,
+  WithRef,
+} from '../utils/dynamic-prop';
+import {
+  excludeProps,
+} from '../utils/exclude-props';
 
 export type TailwindButtonProps<T extends ValidConstructor = 'button'> = {
   as?: T,
   disabled?: boolean;
-} & Omit<DynamicProps<T>, 'as'>
+} & WithRef<T> & Omit<DynamicProps<T>, 'as' | 'disabled' | 'ref'>;
 
 export function TailwindButton<T extends ValidConstructor = 'button'>(
   props: TailwindButtonProps<T>,
 ): JSX.Element {
   const buttonID = createUniqueId();
 
-  const [internalRef, setInternalRef] = createSignal<HTMLElement>();
+  const [internalRef, setInternalRef] = createSignal<DynamicNode<T>>();
 
   createEffect(() => {
     const ref = internalRef();
-    if (ref) {
+    if (ref instanceof HTMLElement) {
       // This behavior is redundant for buttons
       if (ref.tagName !== 'BUTTON') {
         const onKeyDown = (e: KeyboardEvent) => {
@@ -41,22 +58,16 @@ export function TailwindButton<T extends ValidConstructor = 'button'>(
       id={buttonID}
       tabindex={0}
       role="button"
-      disabled={(props.as ?? 'button') === 'button' ? props.disabled : undefined}
+      disabled={props.disabled}
       {...excludeProps(props, [
         'as',
       ])}
       aria-disabled={props.disabled}
       data-sh-disabled={props.disabled}
       data-sh-button={buttonID}
-      ref={(e) => {
-        const outerRef = props.ref;
-        if (typeof outerRef === 'function') {
-          outerRef(e);
-        } else {
-          props.ref = e;
-        }
-        setInternalRef(e);
-      }}
+      ref={createRef(props, (e) => {
+        setInternalRef(() => e);
+      })}
     />
   );
 }
