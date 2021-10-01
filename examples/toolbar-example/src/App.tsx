7cfdb093d730530bb23bc60163e7114954b42730
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-10-01 16:39:34 +0800

    Update `Toolbar` example

--- examples/toolbar-example/src/App.tsx
+++ examples/toolbar-example/src/App.tsx
@@ -2,13 +2,19 @@ import {
   TailwindToggle,
   TailwindToolbar,
 } from 'solid-headless';
-import { JSX, For } from 'solid-js';
+import { JSX, For, createSignal, createSelector } from 'solid-js';
 
 function classNames(...classes: (string | boolean | undefined)[]): string {
   return classes.filter(Boolean).join(' ');
 }
 
-const FONT_OPTIONS = [
+interface ToolbarOption {
+  label: string;
+  title: string;
+  class: string;
+}
+
+const FONT_OPTIONS: ToolbarOption[] = [
   { label: 'B', title: 'Bold', class: 'font-bold' },
   { label: 'I', title: 'Italic', class: 'italic' },
   { label: 'U', title: 'Underlined', class: 'underline' },
@@ -21,17 +27,21 @@ export default function App(): JSX.Element {
       <div class="p-2 bg-white rounded-lg">
         <TailwindToolbar class="flex w-full space-x-1" horizontal>
           <For each={FONT_OPTIONS}>
-            {(item) => (
-              <TailwindToggle
-                class={classNames(
-                  // isSelected(item) ? 'text-color-600 bg-purple-200' : '',
-                  'focus:outline-none focus-visible:ring focus-visible:ring-purple-400 focus-visible:ring-opacity-75',
-                  'w-6 h-6 flex items-center justify-center rounded',
-                )}
-              >
-                <span class={item.class}>{item.label}</span>
-              </TailwindToggle>
-            )}
+            {(item) => {
+              const [checked, setChecked] = createSignal(false);
+              return (
+                <TailwindToggle
+                  class={classNames(
+                    checked() ? 'text-color-600 bg-purple-200' : '',
+                    'focus:outline-none focus-visible:ring focus-visible:ring-purple-400 focus-visible:ring-opacity-75',
+                    'w-6 h-6 flex items-center justify-center rounded transition',
+                  )}
+                  onChange={setChecked}
+                >
+                  <span class={item.class}>{item.label}</span>
+                </TailwindToggle>
+              );
+            }}
           </For>
         </TailwindToolbar>
       </div>
