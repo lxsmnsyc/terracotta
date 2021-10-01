141d0b36275b94076b42f7e4bf6a15bbcb7bd043
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-10-01 14:30:42 +0800

    Add proper ref handling

--- packages/solid-headless/src/utils/dynamic-prop.ts
+++ packages/solid-headless/src/utils/dynamic-prop.ts
@@ -12,6 +12,49 @@ export type DynamicProps<T extends ValidConstructor> =
     ? U
     : never;
 
-export interface WithRef<T> {
-  ref?: T | ((value: T) => void);
+type UnboxIntrinsicElements<T> =
+  T extends JSX.HTMLAttributes<infer U>
+    ? U
+    : never;
+
+type RefCallback<T> = (el: T) => void;
+type RefField<T> = T | RefCallback<T>;
+
+type UnboxComponentProp<U> =
+  U extends { ref: infer X }
+    ? X
+    : never;
+
+export type DynamicNode<T extends ValidConstructor> =
+  T extends ValidElements
+    ? UnboxIntrinsicElements<JSX.IntrinsicElements[T]>
+    :
+  T extends ValidComponent<infer U>
+    ? UnboxComponentProp<U>
+    : never;
+
+export interface WithRef<T extends ValidConstructor> {
+  ref?: RefField<DynamicNode<T>>;
+}
+
+function isRefFunction<U extends ValidConstructor>(
+  callback?: RefField<DynamicNode<U>>,
+): callback is RefCallback<DynamicNode<U>> {
+  return typeof callback === 'function';
+}
+
+export function createRef<U extends ValidConstructor>(
+  props: WithRef<U>,
+  callback: RefCallback<DynamicNode<U>>,
+): RefCallback<DynamicNode<U>> {
+  return (e) => {
+    if ('ref' in props) {
+      if (isRefFunction(props.ref)) {
+        props.ref(e);
+      } else {
+        props.ref = e;
+      }
+    }
+    callback(e);
+  };
 }
