d4e36431139977eb6092981f49a7431aec7b9fa2
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-10-06 13:52:31 +0800

    Init docs

---
+++ docs/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "root": true,
+  "extends": [
+    "lxsmnsyc/typescript/react"
+  ],
+  "parserOptions": {
+    "project": "./tsconfig.eslint.json",
+    "tsconfigRootDir": __dirname,
+  },
+  "rules": {
+  }
+};
