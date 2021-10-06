8de602afc0f78d35245d845983b784e81a98633c
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-10-06 14:16:47 +0800

    Deprecate `Tailwind` prefix

--- packages/solid-headless/src/index.ts
+++ packages/solid-headless/src/index.ts
@@ -44,161 +44,428 @@ export {
   useHeadlessToggleChild,
 } from './headless/Toggle';
 
-// Tailwind
 export {
-  TailwindTransition,
-  TailwindTransitionChild,
-  TailwindTransitionProps,
-  TailwindTransitionChildProps,
-} from './tailwind/Transition';
+  Transition,
+  TransitionChild,
+  TransitionProps,
+  TransitionChildProps,
+} from './components/Transition';
 export {
-  TailwindDisclosure,
-  TailwindDisclosureButton,
-  TailwindDisclosureButtonProps,
-  TailwindDisclosurePanel,
-  TailwindDisclosurePanelProps,
-  TailwindDisclosureProps,
-} from './tailwind/Disclosure';
+  Disclosure,
+  DisclosureButton,
+  DisclosureButtonProps,
+  DisclosurePanel,
+  DisclosurePanelProps,
+  DisclosureProps,
+} from './components/Disclosure';
 export {
-  TailwindAccordion,
-  TailwindAccordionButton,
-  TailwindAccordionButtonProps,
-  TailwindAccordionHeader,
-  TailwindAccordionHeaderProps,
-  TailwindAccordionItem,
-  TailwindAccordionItemProps,
-  TailwindAccordionPanel,
-  TailwindAccordionPanelProps,
-  TailwindAccordionProps,
-} from './tailwind/Accordion';
+  Accordion,
+  AccordionButton,
+  AccordionButtonProps,
+  AccordionHeader,
+  AccordionHeaderProps,
+  AccordionItem,
+  AccordionItemProps,
+  AccordionPanel,
+  AccordionPanelProps,
+  AccordionProps,
+} from './components/Accordion';
 export {
-  TailwindRadioGroup,
-  TailwindRadioGroupDescription,
-  TailwindRadioGroupDescriptionProps,
-  TailwindRadioGroupLabel,
-  TailwindRadioGroupLabelProps,
-  TailwindRadioGroupOption,
-  TailwindRadioGroupOptionProps,
-  TailwindRadioGroupProps,
-} from './tailwind/RadioGroup';
+  RadioGroup,
+  RadioGroupDescription,
+  RadioGroupDescriptionProps,
+  RadioGroupLabel,
+  RadioGroupLabelProps,
+  RadioGroupOption,
+  RadioGroupOptionProps,
+  RadioGroupProps,
+} from './components/RadioGroup';
 export {
-  TailwindListbox,
-  TailwindListboxButton,
-  TailwindListboxButtonProps,
-  TailwindListboxLabel,
-  TailwindListboxLabelProps,
-  TailwindListboxOption,
-  TailwindListboxOptionProps,
-  TailwindListboxOptions,
-  TailwindListboxOptionsProps,
-  TailwindListboxProps,
-} from './tailwind/Listbox';
+  Listbox,
+  ListboxButton,
+  ListboxButtonProps,
+  ListboxLabel,
+  ListboxLabelProps,
+  ListboxOption,
+  ListboxOptionProps,
+  ListboxOptions,
+  ListboxOptionsProps,
+  ListboxProps,
+} from './components/Listbox';
 export {
-  TailwindDialog,
-  TailwindDialogProps,
-  TailwindDialogPanelProps,
-  TailwindDialogPanel,
-  TailwindDialogDescriptionProps,
-  TailwindDialogDescription,
-  TailwindDialogTitle,
-  TailwindDialogTitleProps,
-  TailwindDialogOverlay,
-  TailwindDialogOverlayProps,
-} from './tailwind/Dialog';
+  Dialog,
+  DialogProps,
+  DialogPanelProps,
+  DialogPanel,
+  DialogDescriptionProps,
+  DialogDescription,
+  DialogTitle,
+  DialogTitleProps,
+  DialogOverlay,
+  DialogOverlayProps,
+} from './components/Dialog';
 export {
-  TailwindToolbar,
-  TailwindToolbarProps,
-} from './tailwind/Toolbar';
+  Toolbar,
+  ToolbarProps,
+} from './components/Toolbar';
 export {
-  TailwindSelect,
-  TailwindSelectOption,
-  TailwindSelectOptionProps,
-  TailwindSelectProps,
-} from './tailwind/Select';
+  Select,
+  SelectOption,
+  SelectOptionProps,
+  SelectProps,
+} from './components/Select';
 export {
-  TailwindButton,
-  TailwindButtonProps,
-} from './tailwind/Button';
+  Button,
+  ButtonProps,
+} from './components/Button';
 export {
-  TailwindToggleProps,
-  TailwindToggle,
-} from './tailwind/Toggle';
+  ToggleProps,
+  Toggle,
+} from './components/Toggle';
 export {
-  TailwindPopover,
-  TailwindPopoverButton,
-  TailwindPopoverButtonProps,
-  TailwindPopoverPanel,
-  TailwindPopoverPanelProps,
-  TailwindPopoverProps,
-  TailwindPopoverOverlay,
-  TailwindPopoverOverlayProps,
-} from './tailwind/Popover';
+  Popover,
+  PopoverButton,
+  PopoverButtonProps,
+  PopoverPanel,
+  PopoverPanelProps,
+  PopoverProps,
+  PopoverOverlay,
+  PopoverOverlayProps,
+} from './components/Popover';
 export {
   default as Fragment,
 } from './utils/Fragment';
 export {
-  TailwindAlert,
-  TailwindAlertProps,
-} from './tailwind/Alert';
+  Alert,
+  AlertProps,
+} from './components/Alert';
 export {
-  TailwindAlertDialog,
-  TailwindAlertDialogDescription,
-  TailwindAlertDialogDescriptionProps,
-  TailwindAlertDialogOverlay,
-  TailwindAlertDialogOverlayProps,
-  TailwindAlertDialogPanel,
-  TailwindAlertDialogPanelProps,
-  TailwindAlertDialogProps,
-  TailwindAlertDialogTitle,
-  TailwindAlertDialogTitleProps,
-} from './tailwind/AlertDialog';
+  AlertDialog,
+  AlertDialogDescription,
+  AlertDialogDescriptionProps,
+  AlertDialogOverlay,
+  AlertDialogOverlayProps,
+  AlertDialogPanel,
+  AlertDialogPanelProps,
+  AlertDialogProps,
+  AlertDialogTitle,
+  AlertDialogTitleProps,
+} from './components/AlertDialog';
 export {
-  TailwindToast,
-  TailwindToaster,
-  TailwindToastProps,
-  TailwindToasterProps,
-  ToastData,
+  Toast,
   Toaster,
+  ToastProps,
+  ToasterProps,
+  ToastData,
+  ToasterStore,
   ToasterListener,
   useToaster,
-} from './tailwind/Toast';
+} from './components/Toast';
 export {
-  TailwindCheckbox,
-  TailwindCheckboxDescription,
-  TailwindCheckboxDescriptionProps,
-  TailwindCheckboxIndicator,
-  TailwindCheckboxIndicatorProps,
-  TailwindCheckboxLabel,
-  TailwindCheckboxLabelProps,
-  TailwindCheckboxProps,
-} from './tailwind/Checkbox';
+  Checkbox,
+  CheckboxDescription,
+  CheckboxDescriptionProps,
+  CheckboxIndicator,
+  CheckboxIndicatorProps,
+  CheckboxLabel,
+  CheckboxLabelProps,
+  CheckboxProps,
+} from './components/Checkbox';
 export {
-  TailwindMenu,
-  TailwindMenuItem,
-  TailwindMenuItemProps,
-  TailwindMenuProps,
-} from './tailwind/Menu';
+  Menu,
+  MenuItem,
+  MenuItemProps,
+  MenuProps,
+} from './components/Menu';
 export {
-  TailwindContextMenu,
-  TailwindContextMenuBoundary,
-  TailwindContextMenuBoundaryProps,
-  TailwindContextMenuOverlay,
-  TailwindContextMenuOverlayProps,
-  TailwindContextMenuPanel,
-  TailwindContextMenuPanelProps,
-  TailwindContextMenuProps,
-} from './tailwind/ContextMenu';
+  ContextMenu,
+  ContextMenuBoundary,
+  ContextMenuBoundaryProps,
+  ContextMenuOverlay,
+  ContextMenuOverlayProps,
+  ContextMenuPanel,
+  ContextMenuPanelProps,
+  ContextMenuProps,
+} from './components/ContextMenu';
 export {
-  TailwindFeedArticle,
-  TailwindFeedArticleDescription,
-  TailwindFeedArticleDescriptionProps,
-  TailwindFeedArticleLabel,
-  TailwindFeedArticleLabelProps,
-  TailwindFeedArticleProps,
-  TailwindFeedContent,
-  TailwindFeedContentProps,
-  TailwindFeedLabel,
-  TailwindFeedLabelProps,
-  TailwindFeedProps,
-  TailwindFeed,
-} from './tailwind/Feed';
+  FeedArticle,
+  FeedArticleDescription,
+  FeedArticleDescriptionProps,
+  FeedArticleLabel,
+  FeedArticleLabelProps,
+  FeedArticleProps,
+  FeedContent,
+  FeedContentProps,
+  FeedLabel,
+  FeedLabelProps,
+  FeedProps,
+  Feed,
+} from './components/Feed';
+
+// Tailwind
+export {
+  // @deprecated
+  Transition as TailwindTransition,
+  // @deprecated
+  TransitionChild as TailwindTransitionChild,
+  // @deprecated
+  TransitionProps as TailwindTransitionProps,
+  // @deprecated
+  TransitionChildProps as TailwindTransitionChildProps,
+} from './components/Transition';
+export {
+  // @deprecated
+  Disclosure as TailwindDisclosure,
+  // @deprecated
+  DisclosureButton as TailwindDisclosureButton,
+  // @deprecated
+  DisclosureButtonProps as TailwindDisclosureButtonProps,
+  // @deprecated
+  DisclosurePanel as TailwindDisclosurePanel,
+  // @deprecated
+  DisclosurePanelProps as TailwindDisclosurePanelProps,
+  // @deprecated
+  DisclosureProps as TailwindDisclosureProps,
+} from './components/Disclosure';
+export {
+  // @deprecated
+  Accordion as TailwindAccordion,
+  // @deprecated
+  AccordionButton as TailwindAccordionButton,
+  // @deprecated
+  AccordionButtonProps as TailwindAccordionButtonProps,
+  // @deprecated
+  AccordionHeader as TailwindAccordionHeader,
+  // @deprecated
+  AccordionHeaderProps as TailwindAccordionHeaderProps,
+  // @deprecated
+  AccordionItem as TailwindAccordionItem,
+  // @deprecated
+  AccordionItemProps as TailwindAccordionItemProps,
+  // @deprecated
+  AccordionPanel as TailwindAccordionPanel,
+  // @deprecated
+  AccordionPanelProps as TailwindAccordionPanelProps,
+  // @deprecated
+  AccordionProps as TailwindAccordionProps,
+} from './components/Accordion';
+export {
+  // @deprecated
+  RadioGroup as TailwindRadioGroup,
+  // @deprecated
+  RadioGroupDescription as TailwindRadioGroupDescription,
+  // @deprecated
+  RadioGroupDescriptionProps as TailwindRadioGroupDescriptionProps,
+  // @deprecated
+  RadioGroupLabel as TailwindRadioGroupLabel,
+  // @deprecated
+  RadioGroupLabelProps as TailwindRadioGroupLabelProps,
+  // @deprecated
+  RadioGroupOption as TailwindRadioGroupOption,
+  // @deprecated
+  RadioGroupOptionProps as TailwindRadioGroupOptionProps,
+  // @deprecated
+  RadioGroupProps as TailwindRadioGroupProps,
+} from './components/RadioGroup';
+export {
+  // @deprecated
+  Listbox as TailwindListbox,
+  // @deprecated
+  ListboxButton as TailwindListboxButton,
+  // @deprecated
+  ListboxButtonProps as TailwindListboxButtonProps,
+  // @deprecated
+  ListboxLabel as TailwindListboxLabel,
+  // @deprecated
+  ListboxLabelProps as TailwindListboxLabelProps,
+  // @deprecated
+  ListboxOption as TailwindListboxOption,
+  // @deprecated
+  ListboxOptionProps as TailwindListboxOptionProps,
+  // @deprecated
+  ListboxOptions as TailwindListboxOptions,
+  // @deprecated
+  ListboxOptionsProps as TailwindListboxOptionsProps,
+  // @deprecated
+  ListboxProps as TailwindListboxProps,
+  // @deprecated
+} from './components/Listbox';
+export {
+  // @deprecated
+  Dialog as TailwindDialog,
+  // @deprecated
+  DialogProps as TailwindDialogProps,
+  // @deprecated
+  DialogPanelProps as TailwindDialogPanelProps,
+  // @deprecated
+  DialogPanel as TailwindDialogPanel,
+  // @deprecated
+  DialogDescriptionProps as TailwindDialogDescriptionProps,
+  // @deprecated
+  DialogDescription as TailwindDialogDescription,
+  // @deprecated
+  DialogTitle as TailwindDialogTitle,
+  // @deprecated
+  DialogTitle as TailwindDialogTitleProps,
+  // @deprecated
+  DialogOverlay as TailwindDialogOverlay,
+  // @deprecated
+  DialogOverlayProps as TailwindDialogOverlayProps,
+} from './components/Dialog';
+export {
+  // @deprecated
+  Toolbar as TailwindToolbar,
+  // @deprecated
+  ToolbarProps as TailwindToolbarProps,
+} from './components/Toolbar';
+export {
+  // @deprecated
+  Select as TailwindSelect,
+  // @deprecated
+  SelectOption as TailwindSelectOption,
+  // @deprecated
+  SelectOptionProps as TailwindSelectOptionProps,
+  // @deprecated
+  SelectProps as TailwindSelectProps,
+} from './components/Select';
+export {
+  // @deprecated
+  Button as TailwindButton,
+  // @deprecated
+  ButtonProps as TailwindButtonProps,
+} from './components/Button';
+export {
+  // @deprecated
+  ToggleProps as TailwindToggleProps,
+  // @deprecated
+  Toggle as TailwindToggle,
+} from './components/Toggle';
+export {
+  // @deprecated
+  Popover as TailwindPopover,
+  // @deprecated
+  PopoverButton as TailwindPopoverButton,
+  // @deprecated
+  PopoverButtonProps as TailwindPopoverButtonProps,
+  // @deprecated
+  PopoverPanel as TailwindPopoverPanel,
+  // @deprecated
+  PopoverPanelProps as TailwindPopoverPanelProps,
+  // @deprecated
+  PopoverProps as TailwindPopoverProps,
+  // @deprecated
+  PopoverOverlay as TailwindPopoverOverlay,
+  // @deprecated
+  PopoverOverlayProps as TailwindPopoverOverlayProps,
+} from './components/Popover';
+export {
+  // @deprecated
+  Alert as TailwindAlert,
+  // @deprecated
+  AlertProps as TailwindAlertProps,
+} from './components/Alert';
+export {
+  // @deprecated
+  AlertDialog as TailwindAlertDialog,
+  // @deprecated
+  AlertDialogDescription as TailwindAlertDialogDescription,
+  // @deprecated
+  AlertDialogDescriptionProps as TailwindAlertDialogDescriptionProps,
+  // @deprecated
+  AlertDialogOverlay as TailwindAlertDialogOverlay,
+  // @deprecated
+  AlertDialogOverlayProps as TailwindAlertDialogOverlayProps,
+  // @deprecated
+  AlertDialogPanel as TailwindAlertDialogPanel,
+  // @deprecated
+  AlertDialogPanelProps as TailwindAlertDialogPanelProps,
+  // @deprecated
+  AlertDialogProps as TailwindAlertDialogProps,
+  // @deprecated
+  AlertDialogTitle as TailwindAlertDialogTitle,
+  // @deprecated
+  AlertDialogTitleProps as TailwindAlertDialogTitleProps,
+  // @deprecated
+} from './components/AlertDialog';
+export {
+  // @deprecated
+  Toast as TailwindToast,
+  // @deprecated
+  Toaster as TailwindToaster,
+  // @deprecated
+  ToastProps as TailwindToastProps,
+  // @deprecated
+  ToasterProps as TailwindToasterProps,
+} from './components/Toast';
+export {
+  // @deprecated
+  Checkbox as TailwindCheckbox,
+  // @deprecated
+  CheckboxDescription as TailwindCheckboxDescription,
+  // @deprecated
+  CheckboxDescriptionProps as TailwindCheckboxDescriptionProps,
+  // @deprecated
+  CheckboxIndicator as TailwindCheckboxIndicator,
+  // @deprecated
+  CheckboxIndicatorProps as TailwindCheckboxIndicatorProps,
+  // @deprecated
+  CheckboxLabel as TailwindCheckboxLabel,
+  // @deprecated
+  CheckboxLabelProps as TailwindCheckboxLabelProps,
+  // @deprecated
+  CheckboxProps as TailwindCheckboxProps,
+} from './components/Checkbox';
+export {
+  // @deprecated
+  Menu as TailwindMenu,
+  // @deprecated
+  MenuItem as TailwindMenuItem,
+  // @deprecated
+  MenuItemProps as TailwindMenuItemProps,
+  // @deprecated
+  MenuProps as TailwindMenuProps,
+} from './components/Menu';
+export {
+  // @deprecated
+  ContextMenu as TailwindContextMenu,
+  // @deprecated
+  ContextMenuBoundary as TailwindContextMenuBoundary,
+  // @deprecated
+  ContextMenuBoundaryProps as TailwindContextMenuBoundaryProps,
+  // @deprecated
+  ContextMenuOverlay as TailwindContextMenuOverlay,
+  // @deprecated
+  ContextMenuOverlayProps as TailwindContextMenuOverlayProps,
+  // @deprecated
+  ContextMenuPanel as TailwindContextMenuPanel,
+  // @deprecated
+  ContextMenuPanelProps as TailwindContextMenuPanelProps,
+  // @deprecated
+  ContextMenuProps as TailwindContextMenuProps,
+} from './components/ContextMenu';
+export {
+  // @deprecated
+  FeedArticle as TailwindFeedArticle,
+  // @deprecated
+  FeedArticleDescription as TailwindFeedArticleDescription,
+  // @deprecated
+  FeedArticleDescriptionProps as TailwindFeedArticleDescriptionProps,
+  // @deprecated
+  FeedArticleLabel as TailwindFeedArticleLabel,
+  // @deprecated
+  FeedArticleLabelProps as TailwindFeedArticleLabelProps,
+  // @deprecated
+  FeedArticleProps as TailwindFeedArticleProps,
+  // @deprecated
+  FeedContent as TailwindFeedContent,
+  // @deprecated
+  FeedContentProps as TailwindFeedContentProps,
+  // @deprecated
+  FeedLabel as TailwindFeedLabel,
+  // @deprecated
+  FeedLabelProps as TailwindFeedLabelProps,
+  // @deprecated
+  FeedProps as TailwindFeedProps,
+  // @deprecated
+  Feed as TailwindFeed,
+} from './components/Feed';
