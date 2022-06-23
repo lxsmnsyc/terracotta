378032886bc1408a282344041ab13a7ceadff28f
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-06-23 12:04:24 +0800

    Update dependencies

---
+++ packages/solid-headless/src/utils/focus-start-point.ts
@@ -0,0 +1,17 @@
+export function getFocusStartPoint() {
+  return window.getSelection()?.focusNode?.parentElement;
+}
+
+export function setFocusStartPoint(element: HTMLElement) {
+  const tabindex = element.getAttribute('tabindex');
+
+  element.setAttribute('tabindex', '-1');
+  element.focus();
+  element.blur();
+
+  if (tabindex) {
+    element.setAttribute('tabindex', tabindex);
+  } else {
+    element.removeAttribute('tabindex');
+  }
+}
