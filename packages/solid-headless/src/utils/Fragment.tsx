5c693a718203c8b6fa9a94e7c78b0fd9ed53aa84
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-07-10 14:43:26 +0800

    Update Fragment.tsx

--- packages/solid-headless/src/utils/Fragment.tsx
+++ packages/solid-headless/src/utils/Fragment.tsx
@@ -1,9 +1,9 @@
-import { JSX } from 'solid-js';
+import { createMemo, JSX } from 'solid-js';
 
 export interface FragmentProps {
   children?: JSX.Element;
 }
 
 export default function Fragment(props: FragmentProps): JSX.Element {
-  return props.children;
+  return createMemo(() => props.children);
 }
