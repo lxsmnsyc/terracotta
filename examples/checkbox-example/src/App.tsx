142d0d74d04931c40286da8f006535368c186a8a
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-07-14 19:59:09 +0800

    Update App.tsx

--- examples/checkbox-example/src/App.tsx
+++ examples/checkbox-example/src/App.tsx
@@ -1,16 +1,15 @@
 import {
-  Dialog,
-  DialogPanel,
-  DialogTitle,
-  Transition,
-  TransitionChild,
-  DialogOverlay,
   Checkbox,
   CheckboxIndicator,
   CheckboxLabel,
   CheckboxDescription,
 } from 'solid-headless';
-import { createSignal, JSX, Switch, Match } from 'solid-js';
+import {
+  createSignal,
+  JSX,
+  Switch,
+  Match,
+} from 'solid-js';
 
 function CheckIcon(props: JSX.IntrinsicElements['svg']): JSX.Element {
   return (
@@ -24,7 +23,8 @@ function CheckIcon(props: JSX.IntrinsicElements['svg']): JSX.Element {
       <path
         stroke-linecap="round"
         stroke-linejoin="round"
-        stroke-width="2" d="M5 13l4 4L19 7"
+        stroke-width="2"
+        d="M5 13l4 4L19 7"
       />
     </svg>
   );
@@ -72,42 +72,40 @@ export default function App(): JSX.Element {
   const [checked, setChecked] = createSignal<boolean>();
 
   return (
-    <>
-      <div class="fixed inset-0 flex items-center justify-center">
-        <Checkbox
-          checked={checked()}
-          onChange={setChecked}
-          as="div"
-          class="flex flex-row justify-between items-center space-x-4"
+    <div class="fixed inset-0 flex items-center justify-center">
+      <Checkbox
+        checked={checked()}
+        onChange={setChecked}
+        as="div"
+        class="flex flex-row justify-between items-center space-x-4"
+      >
+        <CheckboxIndicator
+          class="flex-none w-6 h-6 p-1 text-white bg-opacity-25 bg-fuchsia-900 rounded-full focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
         >
-          <CheckboxIndicator
-            class="flex-none w-6 h-6 p-1 text-white bg-opacity-25 bg-fuchsia-900 rounded-full focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-          >
-            <Switch>
-              <Match when={checked() === undefined}>
-                <span class="sr-only">Mixed</span>
-                <UndefinedIcon />
-              </Match>
-              <Match when={checked() === true}>
-                <span class="sr-only">Checked</span>
-                <CheckIcon />
-              </Match>
-              <Match when={checked() === false}>
-                <span class="sr-only">Unchecked</span>
-                <CloseIcon />
-              </Match>
-            </Switch>
-          </CheckboxIndicator>
-          <div class="flex-1 flex flex-col text-white">
-            <CheckboxLabel class="font-bold">
-              This is a checkbox label
-            </CheckboxLabel>
-            <CheckboxDescription class="font-semibold text-xs opacity-50">
-              This is a checkbox description
-            </CheckboxDescription>
-          </div>
-        </Checkbox>
-      </div>
-    </>
+          <Switch>
+            <Match when={checked() === undefined}>
+              <span class="sr-only">Mixed</span>
+              <UndefinedIcon />
+            </Match>
+            <Match when={checked() === true}>
+              <span class="sr-only">Checked</span>
+              <CheckIcon />
+            </Match>
+            <Match when={checked() === false}>
+              <span class="sr-only">Unchecked</span>
+              <CloseIcon />
+            </Match>
+          </Switch>
+        </CheckboxIndicator>
+        <div class="flex-1 flex flex-col text-white">
+          <CheckboxLabel class="font-bold">
+            This is a checkbox label
+          </CheckboxLabel>
+          <CheckboxDescription class="font-semibold text-xs opacity-50">
+            This is a checkbox description
+          </CheckboxDescription>
+        </div>
+      </Checkbox>
+    </div>
   );
 }
