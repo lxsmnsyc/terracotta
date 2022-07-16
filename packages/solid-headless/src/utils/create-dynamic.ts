2b50ddd5c5777962bdf5140c103ac52d70de4452
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-07-16 19:53:11 +0800

    Fix SSR

--- packages/solid-headless/src/utils/create-dynamic.ts
+++ packages/solid-headless/src/utils/create-dynamic.ts
@@ -1,39 +1,21 @@
 import {
-  Accessor,
-  createMemo,
+  createComponent,
   JSX,
-  sharedConfig,
-  untrack,
+  mergeProps,
 } from 'solid-js';
-import {
-  getNextElement,
-  SVGElements,
-  spread,
-} from 'solid-js/web';
+import { Dynamic } from 'solid-js/web';
 import {
   DynamicProps,
   ValidConstructor,
 } from './dynamic-prop';
 
-const SVG_NAMESPACE = 'http://www.w3.org/2000/svg';
-
-function createElement(tagName: string, isSVG = false): HTMLElement | SVGElement {
-  return isSVG ? document.createElementNS(SVG_NAMESPACE, tagName) : document.createElement(tagName);
-}
-
 export default function createDynamic<T extends ValidConstructor>(
   source: () => T,
   props: DynamicProps<T>,
-): Accessor<JSX.Element> {
-  const cached = createMemo(source);
-  return createMemo(() => {
-    const component = cached();
-    if (typeof component === 'function') {
-      return untrack(() => component(props));
-    }
-    const isSvg = SVGElements.has(component);
-    const el = sharedConfig.context ? getNextElement() : createElement(component, isSvg);
-    spread(el, props, isSvg);
-    return el;
-  });
+): JSX.Element {
+  return createComponent(Dynamic, mergeProps({
+    get component() {
+      return source();
+    },
+  }, props) as any);
 }
