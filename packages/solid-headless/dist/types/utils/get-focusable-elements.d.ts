757d2712579b5d9dbe970d16623ba296349507e9
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-07-20 18:29:25 +0800

    Delete get-focusable-elements.d.ts

--- packages/solid-headless/dist/types/utils/get-focusable-elements.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function isFocusable<T extends HTMLElement>(element: T): boolean;
-export default function getFocusableElements(node: HTMLElement, filter?: HTMLElement): HTMLElement[];
