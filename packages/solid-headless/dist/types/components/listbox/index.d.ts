f5097295fcfbf599eeb78e83569d803f966fcc05
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-07-28 19:45:16 +0800

    Fix dist

--- packages/solid-headless/dist/types/components/listbox/index.d.ts
+++ packages/solid-headless/dist/types/components/listbox/index.d.ts
@@ -1,5 +1,13 @@
-export { Listbox, ListboxProps, } from './Listbox';
+export { Listbox, ListboxProps, ListboxMultipleProps, ListboxSingleProps, } from './Listbox';
 export { ListboxButton, ListboxButtonProps, } from './ListboxButton';
 export { ListboxLabel, ListboxLabelProps, } from './ListboxLabel';
 export { ListboxOption, ListboxOptionProps, } from './ListboxOption';
 export { ListboxOptions, ListboxOptionsProps, } from './ListboxOptions';
+export { ListboxMCSCDProps, } from './ListboxMCSCD';
+export { ListboxMCSUDProps, } from './ListboxMCSUD';
+export { ListboxMUSCDProps, } from './ListboxMUSCD';
+export { ListboxMUSUDProps, } from './ListboxMUSUD';
+export { ListboxSCSCDProps, } from './ListboxSCSCD';
+export { ListboxSCSUDProps, } from './ListboxSCSUD';
+export { ListboxSUSCDProps, } from './ListboxSUSCD';
+export { ListboxSUSUDProps, } from './ListboxSUSUD';
