f98b0ddf2852c17975c59c155f63818d6f55c2f6
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-03-31 17:56:33 +0800

    Rework `Toggle`

--- packages/solid-headless/src/components/toggle/index.ts
+++ packages/solid-headless/src/components/toggle/index.ts
@@ -1,23 +1,48 @@
 import {
+  createEffect,
+  onCleanup,
   createComponent,
+  mergeProps,
   JSX,
+  createMemo,
 } from 'solid-js';
 import {
+  omitProps,
+} from 'solid-use/props';
+import {
+  createToggleState,
+  ToggleStateControlledOptions,
+  ToggleStateProvider,
+  ToggleStateProviderRenderProps,
+  ToggleStateUncontrolledOptions,
+} from '../../states/create-toggle-state';
+import {
+  createForwardRef,
+  DynamicProps,
+  HeadlessPropsWithRef,
   ValidConstructor,
 } from '../../utils/dynamic-prop';
+import { createDisabled } from '../../utils/state-props';
+import { OmitAndMerge } from '../../utils/types';
 import {
-  ToggleControlled,
-  ToggleControlledProps,
-} from './ToggleControlled';
-import {
-  ToggleUncontrolled,
-  ToggleUncontrolledProps,
-} from './ToggleUncontrolled';
+  Button,
+  ButtonProps,
+} from '../button';
+import { TOGGLE_TAG } from './tags';
 
-export {
-  ToggleUncontrolledProps,
-  ToggleControlledProps,
-};
+export type ToggleControlledBaseProps =
+  ToggleStateControlledOptions
+  & ToggleStateProviderRenderProps;
+
+export type ToggleControlledProps<T extends ValidConstructor = 'button'> =
+  HeadlessPropsWithRef<T, OmitAndMerge<ToggleControlledBaseProps, ButtonProps<T>>>;
+
+export type ToggleUncontrolledBaseProps =
+  ToggleStateUncontrolledOptions
+  & ToggleStateProviderRenderProps;
+
+export type ToggleUncontrolledProps<T extends ValidConstructor = 'button'> =
+  HeadlessPropsWithRef<T, OmitAndMerge<ToggleUncontrolledBaseProps, ButtonProps<T>>>;
 
 export type ToggleProps<T extends ValidConstructor = 'button'> =
   | ToggleControlledProps<T>
@@ -30,10 +55,71 @@ function isToggleUncontrolled<T extends ValidConstructor = 'button'>(
 }
 
 export function Toggle<T extends ValidConstructor = 'button'>(
+  props: ToggleControlledProps<T>,
+): JSX.Element;
+export function Toggle<T extends ValidConstructor = 'button'>(
+  props: ToggleUncontrolledProps<T>,
+): JSX.Element
+export function Toggle<T extends ValidConstructor = 'button'>(
   props: ToggleProps<T>,
 ): JSX.Element {
-  if (isToggleUncontrolled(props)) {
-    return createComponent(ToggleUncontrolled, props);
-  }
-  return createComponent(ToggleControlled, props);
+  const [ref, setRef] = createForwardRef(props);
+
+  return createMemo(() => {
+    const state = createToggleState(props);
+
+    createEffect(() => {
+      const current = ref();
+      if (current instanceof HTMLElement) {
+        const onClick = () => {
+          state.setState(!state.pressed);
+        };
+
+        current.addEventListener('click', onClick);
+        onCleanup(() => {
+          current.removeEventListener('click', onClick);
+        });
+      }
+    });
+
+    return createComponent(Button, mergeProps(
+
+      isToggleUncontrolled(props)
+        ? omitProps(props, [
+          'as',
+          'onChange',
+          'defaultPressed',
+          'ref',
+          'disabled',
+          'children',
+        ])
+        : omitProps(props, [
+          'as',
+          'onChange',
+          'pressed',
+          'ref',
+          'disabled',
+          'children',
+        ]),
+      TOGGLE_TAG,
+      {
+        ref: setRef,
+        get 'aria-pressed'() {
+          return props.pressed;
+        },
+        get 'data-sh-pressed'() {
+          return props.pressed;
+        },
+        get children() {
+          return createComponent(ToggleStateProvider, {
+            state,
+            get children() {
+              return props.children;
+            },
+          });
+        },
+      },
+      createDisabled(() => state.disabled()),
+    ) as DynamicProps<T>);
+  }) as unknown as JSX.Element;
 }
