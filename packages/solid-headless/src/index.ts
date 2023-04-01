e9efccb78ad8dcd63fdc85f1adc5f225dc963627
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-04-01 15:45:41 +0800

    Update index.ts

--- packages/solid-headless/src/index.ts
+++ packages/solid-headless/src/index.ts
@@ -1,20 +1,4 @@
-export * from './headless/disclosure';
-export * from './headless/select';
-export * from './headless/toggle';
-export * from './headless/input';
-export * from './headless/autocomplete';
-
-export * from './components/accordion';
-export * from './components/alert';
-export * from './components/alert-dialog';
 export * from './components/button';
-export * from './components/checkbox';
-export * from './components/color-scheme';
-export * from './components/command-bar';
-export * from './components/context-menu';
-export * from './components/dialog';
-export * from './components/disclosure';
-export * from './components/feed';
 export * from './components/listbox';
 export * from './components/menu';
 export * from './components/popover';
@@ -25,3 +9,76 @@ export * from './components/toast';
 export * from './components/toggle';
 export * from './components/toolbar';
 export * from './components/transition';
+
+export {
+  AutocompleteStateChild,
+  AutocompleteStateControlledOptions,
+  AutocompleteStateOptions,
+  AutocompleteStateProperties,
+  AutocompleteStateRenderProps,
+  AutocompleteStateUncontrolledOptions,
+  useAutocompleteState,
+  createAutocompleteState,
+} from './states/create-autocomplete-state';
+export {
+  CheckStateChild,
+  CheckStateControlledOptions,
+  CheckStateOptions,
+  CheckStateProperties,
+  CheckStateRenderProps,
+  CheckStateUncontrolledOptions,
+  createCheckState,
+  useCheckState,
+} from './states/create-check-state';
+export {
+  DisclosureStateChild,
+  DisclosureStateControlledOptions,
+  DisclosureStateOptions,
+  DisclosureStateProperties,
+  DisclosureStateRenderProps,
+  DisclosureStateUncontrolledOptions,
+  useDisclosureState,
+  createDisclosureState,
+} from './states/create-disclosure-state';
+export {
+  InputStateChild,
+  InputStateControlledOptions,
+  InputStateOptions,
+  InputStateProperties,
+  InputStateRenderProps,
+  InputStateUncontrolledOptions,
+  useInputState,
+  createInputState,
+} from './states/create-input-state';
+export {
+  SelectOptionStateChild,
+  SelectOptionStateOptions,
+  SelectOptionStateProperties,
+  SelectOptionStateRenderProps,
+  useSelectOptionState,
+  createSelectOptionState,
+} from './states/create-select-option-state';
+export {
+  SelectStateChild,
+  SelectStateProperties,
+  SelectStateRenderProps,
+  SingleSelectStateControlledOptions,
+  SingleSelectStateOptions,
+  SingleSelectStateUncontrolledOptions,
+  useSelectState,
+  createSingleSelectState,
+  MultipleSelectStateControlledOptions,
+  MultipleSelectStateOptions,
+  MultipleSelectStateUncontrolledOptions,
+  createMultipleSelectState,
+} from './states/create-select-state';
+export {
+  ToggleStateChild,
+  ToggleStateControlledOptions,
+  ToggleStateOptions,
+  ToggleStateProperties,
+  ToggleStateRenderProps,
+  ToggleStateUncontrolledOptions,
+  useToggleState,
+  createToggleState,
+} from './states/create-toggle-state';
