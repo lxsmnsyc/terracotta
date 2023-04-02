9d4bef52af8ece2a864266222ab47e221c8d6e15
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-04-02 14:29:16 +0800

    Update `Select` demo

---
+++ examples/select/src/SingleSelect.tsx
@@ -0,0 +1,64 @@
+import {
+  Select,
+  SelectOption,
+} from 'terracotta';
+import { createSignal, JSX, For } from 'solid-js';
+import { CheckIcon, classNames } from './utils';
+
+const people = [
+  { name: 'Wade Cooper' },
+  { name: 'Arlene Mccoy' },
+  { name: 'Devon Webb' },
+  { name: 'Tom Cook' },
+  { name: 'Tanya Fox' },
+  { name: 'Hellen Schmidt' },
+];
+
+export default function SingleSelect(): JSX.Element {
+  const [selected, setSelected] = createSignal(people[0]);
+
+  return (
+    <div class="flex flex-col gap-2">
+      <span class="text-xl font-semibold">Single Selection</span>
+      <Select toggleable value={selected()} onChange={setSelected}>
+        <div class="relative mt-1 bg-gray-50 bg-opacity-50 rounded-lg overflow-hidden">
+          <For each={people}>
+            {(person) => (
+              <SelectOption class="focus:outline-none group" value={person}>
+                {({ isActive, isSelected }) => (
+                  <div
+                    class={classNames(
+                      isActive() ? 'text-amber-900 bg-amber-100' : 'text-gray-900',
+                      'group-hover:text-amber-900 group-hover:bg-amber-100',
+                      'cursor-default select-none relative py-2 pl-10 pr-4',
+                    )}
+                  >
+                    <span
+                      class={classNames(
+                        isSelected() ? 'font-medium' : 'font-normal',
+                        'block truncate',
+                      )}
+                    >
+                      {person.name}
+                    </span>
+                    {isSelected() ? (
+                      <span
+                        class={classNames(
+                          isActive() ? 'text-amber-600' : 'text-amber-600',
+                          'group-hover:text-amber-600',
+                          'absolute inset-y-0 left-0 flex items-center pl-3',
+                        )}
+                      >
+                        <CheckIcon class="w-5 h-5" aria-hidden="true" />
+                      </span>
+                    ) : null}
+                  </div>
+                )}
+              </SelectOption>
+            )}
+          </For>
+        </div>
+      </Select>
+    </div>
+  );
+}
--- examples/select/src/main.tsx
+++ examples/select/src/main.tsx
@@ -1,13 +1,14 @@
 import { render } from 'solid-js/web';
-import App from './App';
-
 import './style.css';
+import SingleSelect from './SingleSelect';
+import MultiSelect from './MultiSelect';
 
 function Root() {
   return (
     <div class="bg-gradient-to-r from-amber-300 to-orange-500 w-screen h-screen flex overflow-hidden">
-      <div class="flex flex-col items-center justify-center w-full">
-        <App />
+      <div class="flex gap-4 items-center justify-center w-full">
+        <SingleSelect />
+        <MultiSelect />
       </div>
     </div>
   );
