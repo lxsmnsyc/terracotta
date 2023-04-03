7ab693d1bba69ef97da032859ca9ee1498410689
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-04-03 23:19:29 +0800

    Add initial demo

---
+++ examples/command/src/SingleSelect.tsx
@@ -0,0 +1,74 @@
+import {
+  Command,
+  CommandInput,
+  CommandOptions,
+  CommandOption,
+  CommandLabel,
+} from 'terracotta';
+import { createSignal, JSX, For } from 'solid-js';
+import { CheckIcon, classNames } from './utils';
+
+const people = [
+  { name: 'Wade Cooper' },
+  { name: 'Arlene Mccoy' },
+  { name: 'Devon Webb' },
+  { name: 'Tom Cook' },
+  { name: 'Tanya Fox' },
+  { name: 'Hellen Schmidt' },
+];
+
+export default function SingleSelect(): JSX.Element {
+  const [selected, setSelected] = createSignal(people[0]);
+
+  return (
+    <div class="flex flex-col gap-2">
+      <Command toggleable value={selected()} onChange={setSelected}>
+        <CommandLabel class="text-xl font-semibold">Single Selection</CommandLabel>
+        <CommandInput
+          class="w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm"
+          placeholder="Select an item"
+          value={selected()?.name ?? ''}
+        />
+        <CommandOptions>
+          <div class="bg-gray-50 bg-opacity-50 rounded-lg overflow-hidden">
+            <For each={people}>
+              {(person) => (
+                <CommandOption class="focus:outline-none group" value={person}>
+                  {({ isActive, isSelected }) => (
+                    <div
+                      class={classNames(
+                        isActive() ? 'text-amber-900 bg-amber-100' : 'text-gray-900',
+                        'group-hover:text-amber-900 group-hover:bg-amber-100',
+                        'cursor-default select-none relative py-2 pl-10 pr-4',
+                      )}
+                    >
+                      <span
+                        class={classNames(
+                          isSelected() ? 'font-medium' : 'font-normal',
+                          'block truncate',
+                        )}
+                      >
+                        {person.name}
+                      </span>
+                      {isSelected() ? (
+                        <span
+                          class={classNames(
+                            isActive() ? 'text-amber-600' : 'text-amber-600',
+                            'group-hover:text-amber-600',
+                            'absolute inset-y-0 left-0 flex items-center pl-3',
+                          )}
+                        >
+                          <CheckIcon class="w-5 h-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </div>
+                  )}
+                </CommandOption>
+              )}
+            </For>
+          </div>
+        </CommandOptions>
+      </Command>
+    </div>
+  );
+}
