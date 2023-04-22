3d750b9f9e2291901a36e0ab665e85e7c3833433
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-04-22 20:16:49 +0800

    Update index.ts

--- packages/terracotta/src/components/alert/index.ts
+++ packages/terracotta/src/components/alert/index.ts
@@ -1,13 +1,13 @@
+import type { JSX } from 'solid-js';
 import {
   createUniqueId,
-  JSX,
   mergeProps,
 } from 'solid-js';
 import {
   omitProps,
 } from 'solid-use/props';
 import createDynamic from '../../utils/create-dynamic';
-import {
+import type {
   ValidConstructor,
   HeadlessProps,
   DynamicProps,
