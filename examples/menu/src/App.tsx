113dd98fe721b878cc0fbf260b05043ee3f0675e
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-01-21 11:18:16 +0800

    Format `Menu`

--- examples/menu/src/App.tsx
+++ examples/menu/src/App.tsx
@@ -1,7 +1,4 @@
-import {
-  Menu,
-  MenuItem,
-} from 'terracotta';
+import { Menu, MenuItem } from 'terracotta';
 import type { JSX } from 'solid-js';
 
 function Separator(): JSX.Element {
@@ -16,24 +13,42 @@ export default function App(): JSX.Element {
   return (
     <div class="w-full flex items-center justify-center">
       <Menu class="overflow-hidden w-64 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white flex flex-col space-y-1 p-1">
-        <MenuItem as="button" class="text-sm p-1 text-left rounded hover:bg-purple-600 hover:text-white focus:outline-none focus:bg-purple-600 focus:text-white">
+        <MenuItem
+          as="button"
+          class="text-sm p-1 text-left rounded hover:bg-purple-600 hover:text-white focus:outline-none focus:bg-purple-600 focus:text-white"
+        >
           Open Link in New Tab
         </MenuItem>
-        <MenuItem as="button" class="text-sm p-1 text-left rounded hover:bg-purple-600 hover:text-white focus:outline-none focus:bg-purple-600 focus:text-white">
+        <MenuItem
+          as="button"
+          class="text-sm p-1 text-left rounded hover:bg-purple-600 hover:text-white focus:outline-none focus:bg-purple-600 focus:text-white"
+        >
           Open Link in New Window
         </MenuItem>
-        <MenuItem as="button" class="text-sm p-1 text-left rounded hover:bg-purple-600 hover:text-white focus:outline-none focus:bg-purple-600 focus:text-white">
+        <MenuItem
+          as="button"
+          class="text-sm p-1 text-left rounded hover:bg-purple-600 hover:text-white focus:outline-none focus:bg-purple-600 focus:text-white"
+        >
           Open Link in New Incognito Window
         </MenuItem>
         <Separator />
-        <MenuItem as="button" class="text-sm p-1 text-left rounded hover:bg-purple-600 hover:text-white focus:outline-none focus:bg-purple-600 focus:text-white">
+        <MenuItem
+          as="button"
+          class="text-sm p-1 text-left rounded hover:bg-purple-600 hover:text-white focus:outline-none focus:bg-purple-600 focus:text-white"
+        >
           Save Link As...
         </MenuItem>
-        <MenuItem as="button" class="text-sm p-1 text-left rounded hover:bg-purple-600 hover:text-white focus:outline-none focus:bg-purple-600 focus:text-white">
+        <MenuItem
+          as="button"
+          class="text-sm p-1 text-left rounded hover:bg-purple-600 hover:text-white focus:outline-none focus:bg-purple-600 focus:text-white"
+        >
           Copy Link Address
         </MenuItem>
         <Separator />
-        <MenuItem as="button" class="text-sm p-1 text-left rounded hover:bg-purple-600 hover:text-white focus:outline-none focus:bg-purple-600 focus:text-white">
+        <MenuItem
+          as="button"
+          class="text-sm p-1 text-left rounded hover:bg-purple-600 hover:text-white focus:outline-none focus:bg-purple-600 focus:text-white"
+        >
           Inspect
         </MenuItem>
       </Menu>
