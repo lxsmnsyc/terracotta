78b3b595fe84e41c656fb191779c97607af13155
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-01-21 11:10:31 +0800

    Format `Combobox`

--- examples/combobox/src/SingleExample.tsx
+++ examples/combobox/src/SingleExample.tsx
@@ -36,7 +36,9 @@ export default function SingleExample(): JSX.Element {
         onSelectChange={setSelected}
         matchBy={matchBy}
       >
-        <ComboboxLabel class="text-xl font-semibold">Single Selection</ComboboxLabel>
+        <ComboboxLabel class="text-xl font-semibold">
+          Single Selection
+        </ComboboxLabel>
         <ComboboxInput
           class="w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm"
           placeholder="Select an item"
@@ -54,14 +56,22 @@ export default function SingleExample(): JSX.Element {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <ComboboxOptions unmount={false} class="absolute w-full bg-gray-50 bg-opacity-50 rounded-lg overflow-hidden">
+              <ComboboxOptions
+                unmount={false}
+                class="absolute w-full bg-gray-50 bg-opacity-50 rounded-lg overflow-hidden"
+              >
                 <For each={people}>
                   {(person): JSX.Element => (
-                    <ComboboxOption class="focus:outline-none group" value={person}>
+                    <ComboboxOption
+                      class="focus:outline-none group"
+                      value={person}
+                    >
                       {({ isActive, isSelected, matches }): JSX.Element => (
                         <div
                           class={classNames(
-                            isActive() ? 'text-amber-900 bg-amber-100' : 'text-gray-900',
+                            isActive()
+                              ? 'text-amber-900 bg-amber-100'
+                              : 'text-gray-900',
                             'group-hover:text-amber-900 group-hover:bg-amber-100',
                             'cursor-default select-none relative py-2 pl-10 pr-4',
                             matches() ? 'visible' : 'hidden',
@@ -78,12 +88,18 @@ export default function SingleExample(): JSX.Element {
                           {isSelected() ? (
                             <span
                               class={classNames(
-                                isActive() ? 'text-amber-600' : 'text-amber-600',
+                                isActive()
+                                  ? 'text-amber-600'
+                                  : 'text-amber-600',
                                 'group-hover:text-amber-600',
                                 'absolute inset-y-0 left-0 flex items-center pl-3',
                               )}
                             >
-                              <CheckIcon class="w-5 h-5" aria-hidden="true" />
+                              <CheckIcon
+                                title="Selected"
+                                class="w-5 h-5"
+                                aria-hidden="true"
+                              />
                             </span>
                           ) : null}
                         </div>
