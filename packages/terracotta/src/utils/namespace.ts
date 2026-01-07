f10d5de4e41f17b1cc976e5df4d88d1a50848625
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2026-01-07 13:30:45 +0800

    Update namespace.ts

--- packages/terracotta/src/utils/namespace.ts
+++ packages/terracotta/src/utils/namespace.ts
@@ -6,11 +6,11 @@ export const CHECKED_NODE = `[${DATA_SET_NAMESPACE}-checked]`;
 export const MATCHES_NODE = `[${DATA_SET_NAMESPACE}-matches]`;
 
 type TerracottaTag = {
-  [key in `${typeof DATA_SET_NAMESPACE}-${string}`]: boolean;
+  [key in `${typeof DATA_SET_NAMESPACE}-${string}`]: string;
 };
 
 export function createTag(tag: string): TerracottaTag {
   return {
-    [`${DATA_SET_NAMESPACE}-${tag}`]: true,
+    [`${DATA_SET_NAMESPACE}-${tag}`]: '',
   };
 }
