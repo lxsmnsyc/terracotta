0d0d5aa790223e0e6bedf8fdbfc5e14ceff07c7b
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2026-01-07 13:19:46 +0800

    Rework component tag

--- packages/terracotta/src/utils/namespace.ts
+++ packages/terracotta/src/utils/namespace.ts
@@ -1,18 +1,16 @@
 export const DATA_SET_NAMESPACE = 'tc';
 
-export const TYPE_ATTRIBUTE = `${DATA_SET_NAMESPACE}-type` as const;
-
 export const DISABLED_NODE = `[${DATA_SET_NAMESPACE}-disabled]`;
 export const SELECTED_NODE = `[${DATA_SET_NAMESPACE}-selected]`;
 export const CHECKED_NODE = `[${DATA_SET_NAMESPACE}-checked]`;
 export const MATCHES_NODE = `[${DATA_SET_NAMESPACE}-matches]`;
 
 type TerracottaTag = {
-  [key in typeof TYPE_ATTRIBUTE]: string;
+  [key in `${typeof DATA_SET_NAMESPACE}-${string}`]: boolean;
 };
 
 export function createTag(tag: string): TerracottaTag {
   return {
-    [TYPE_ATTRIBUTE]: tag,
+    [`${DATA_SET_NAMESPACE}-${tag}`]: true,
   };
 }
