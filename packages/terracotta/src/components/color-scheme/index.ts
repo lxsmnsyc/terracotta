725801ac79439886d2f23ea510d5c3128b49cac1
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2026-03-19 01:03:49 +0800

    Fix `color-scheme`

--- packages/terracotta/src/components/color-scheme/index.ts
+++ packages/terracotta/src/components/color-scheme/index.ts
@@ -74,9 +74,7 @@ export function ColorSchemeProvider(
 
   // Since storage events only work for other windows
   // we need to make the main window sync
-  createEffect(() => {
-    isVisible();
-
+  createEffect(isVisible, () => {
     const onChange = (): void => {
       const value = localStorage.getItem(STORAGE_KEY);
 
@@ -87,20 +85,20 @@ export function ColorSchemeProvider(
       }
     };
     onChange();
-    useEventListener(window, 'storage', onChange, false);
+    return useEventListener(window, 'storage', onChange, false);
   });
 
   // Sync storage when signal changes
-  createEffect(() => {
-    localStorage.setItem(STORAGE_KEY, get());
+  createEffect(get, value => {
+    localStorage.setItem(STORAGE_KEY, value);
   });
 
   // Sync document class
-  createEffect(() => {
-    document.documentElement.classList.toggle('dark', shouldToggle());
+  createEffect(shouldToggle, value => {
+    document.documentElement.classList.toggle('dark', value);
   });
 
-  return createComponent(ColorSchemeContext.Provider, {
+  return createComponent(ColorSchemeContext, {
     value: {
       get value() {
         return get();
