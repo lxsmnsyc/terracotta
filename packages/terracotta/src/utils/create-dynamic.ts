d98d229ffdd07b486b5b544f6be32ccd7f28ac9e
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2026-03-19 17:05:08 +0800

    Update create-dynamic.ts

--- packages/terracotta/src/utils/create-dynamic.ts
+++ packages/terracotta/src/utils/create-dynamic.ts
@@ -1,22 +1,3 @@
-import { Dynamic } from '@solidjs/web';
-import type { JSX, ValidComponent } from 'solid-js';
-import { createComponent, merge } from 'solid-js';
-import type { DynamicProps } from './dynamic-prop';
+import { createDynamic } from '@solidjs/web';
 
-// This is just a shorthand for creating dynamic components
-export default function createDynamic<T extends ValidComponent>(
-  source: () => T,
-  props: DynamicProps<T>,
-): JSX.Element {
-  return createComponent(
-    Dynamic,
-    merge(
-      {
-        get component() {
-          return source();
-        },
-      },
-      props,
-    ) as any,
-  );
-}
+export default createDynamic;
